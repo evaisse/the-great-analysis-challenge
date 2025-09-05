834eacb8c421a8817bead6048d5188b85f4433ed
Emmanuel VAÏSSE <evaisse@gmail.com>
2025-09-05 23:55:56 +0200

    chore: remove go implementation
    gemini commit

--- go/internal/game/ai.go
+++
@@ -1,7 +0,0 @@
-package game
-
-// FindBestMove finds the best move for the current player.
-func (b *Board) FindBestMove(depth int) string {
-	// TODO: Implement minimax with alpha-beta pruning
-	return ""
-}
